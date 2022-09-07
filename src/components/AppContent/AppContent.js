28174724569b397f67f7e16d25b394aaadb0aa7d
Kate Pryshchepava <>
2022-09-07 16:00:37 +0300

    task1

---
+++ src/components/AppContent/AppContent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Content } from 'carbon-components-react';
+import Card from '../Card/Card';
+
+const AppContent = () => (
+    <Content>
+        <div className="bx--grid">
+            <div className="bx--row">
+                <div className="bx--col-sm-6 bx--col-md-4 bx--col-lg-3">
+                    <Card/>
+                </div>
+            </div>
+        </div>
+    </Content>
+);
+
+export default AppContent;
---
+++ src/components/Card/Card.js
@@ -0,0 +1,8 @@
+import { Tile } from 'carbon-components-react';
+import React from 'react';
+
+const Card = () => (
+    <Tile className='bx--tile--clickable card'/>
+);
+
+export default Card;
