263645c6eb435b641a791e083d3573bde6a69f9f
Kate Pryshchepava <>
2022-09-08 10:30:51 +0300

    task1 added card header and content

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,7 +2,15 @@ import { Tile } from 'carbon-components-react';
 import React from 'react';
 
 const Card = () => (
-    <Tile className='bx--tile--clickable card'/>
+    <Tile className='bx--tile--clickable card'>
+        <div className='card__header'>
+            Header
+        </div>
+        <hr/>
+        <div className='card__content'>
+            Content
+        </div>
+    </Tile>
 );
 
 export default Card;
