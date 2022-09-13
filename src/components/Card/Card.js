80e6d1e67a2a819e3f5acbad71700fd92f71f3dc
Kate Pryshchepava <>
2022-09-13 16:11:39 +0300

    task2

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,16 +1,28 @@
 import { Tile } from 'carbon-components-react';
-import React from 'react';
+import React, { useState } from 'react';
 
-const Card = () => (
-    <Tile className='bx--tile--clickable card'>
+
+const Card = () => {
+    const uncheckedCardStyle = 'bx--tile--clickable card';
+    const checkedCardStyle = 'bx--tile--clickable card__checked';
+    
+    const [cardStyleValue, setCardStyleValue] = useState(uncheckedCardStyle);
+
+    function checkBoxClickHandler(event) {
+        setCardStyleValue(event.target.checked ? checkedCardStyle : uncheckedCardStyle);
+    }
+
+    return (
+    <Tile className={cardStyleValue}>
         <div className='card__header'>
             Header
+            <input className='card__checkbox' type="checkbox" onChange={checkBoxClickHandler}/>
         </div>
         <hr/>
         <div className='card__content'>
             Content
         </div>
     </Tile>
-);
+)};
 
 export default Card;
